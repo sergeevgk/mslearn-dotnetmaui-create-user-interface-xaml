bdb3ef8fe264b03b42dc61d218199e53844b7a57
sergeevgk <gosha199904@gmail.com>
2024-01-08 22:49:34 +0300

    1. Add static class with shared resources: font and background color. 2. Add project namespace to XAML files. 3. Use Static and OnPlatform XAML markup extensions to vary height, font and background colors of some control elements.

---
+++ exercise1/Notes/SharedResources.cs
@@ -0,0 +1,7 @@
+﻿namespace Notes;
+
+static class SharedResources
+{
+	public static readonly Color FontColor = Color.FromRgb(108, 70, 78);
+	public static readonly Color BackgroundColor = Color.FromRgb(68, 207, 108);
+}
